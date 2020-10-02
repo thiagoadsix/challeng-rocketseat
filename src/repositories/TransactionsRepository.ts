e30170a96ec0d6adbcfee06b0c355aff02c4d4d8
Thiago Andrade Silva <thiago.adsix@gmail.com>
2020-10-02 00:04:10 -0300

    :construction: Repository Transaction implemented

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,29 @@
+import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;

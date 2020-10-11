06117f4030afaf61edbd7e90f6527d99e296c064
Thiago Andrade Silva <thiago.adsix@gmail.com>
2020-10-11 17:30:31 -0300

    :art: Cors added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 
 import './database/index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
